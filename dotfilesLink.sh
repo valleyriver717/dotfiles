6db9746f00f98c3492203f3fd3259168e0290467
Kenji Tanigawa <taney_mouse@Kenji-no-MacBook.local>
2017-07-20 22:11:47 +0900

    delete dotfilesLink.sh

--- dotfilesLink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/colors ~/.vim
-ln -sf ~/dotfiles/plugin ~/.vim
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.Xresources-hybrid ~/.Xresources-hybrid
-
